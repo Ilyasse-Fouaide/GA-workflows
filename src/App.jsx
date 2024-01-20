1f90720cb52144fb29ac645c3885ca699b1ff74e
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2024-01-20 09:58:54 +0100

    adding tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <h1 className='text-3xl font-bold underline'>App</h1>
       <p>Feature-1</p>
       <p>Feature-2</p>
     </div>
